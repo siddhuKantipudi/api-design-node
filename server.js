cd343014a233403dc269947ed34b955e1c457094
skanti187 <Siddhartha_Kantipudi@cable.comcast.com>
2021-03-16 23:23:09 -0400

    step1 solution

--- server.js
+++ server.js
@@ -3,3 +3,29 @@
 // it should then send back jsonData on a GET to /data
 
 var jsonData = {count: 12, message: 'hey'};
+
+const express = require('express');
+const fs = require('fs');
+const app = express();
+
+app.get('/', (req, res) => {
+  
+  fs.readFile('index.html', (err, buffer) => {
+    const html = buffer.toString();
+    res.setHeader("Content-Type", "text/html");
+    res.send(html);
+  })
+  // res.sendFile(__dirname + '/index.html', (error) => {
+  //   if(error) {
+  //     res.status(500).send(error);
+  //   }
+  // })
+});
+
+app.get('/data', (req, res) => {
+  return res.json(jsonData);
+});
+
+app.listen(3000, function() {
+  console.log("server running on port:3000");
+});
