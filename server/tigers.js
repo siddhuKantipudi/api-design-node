2dfc79c66e36897567c50f3b4219b67dfcd33e3f
Sid Kantipudi <siddhu.kantipudi@gmail.com>
2021-03-29 23:51:21 -0400

    step 4 solution

--- server/tigers.js
+++ server/tigers.js
@@ -2,3 +2,12 @@
 // and make some REST routes for it, exactly like for lions
 // make a middleware that just logs the word 'tiger' to the console
 // when a request comes in to the server
+
+var tigerRouter = require('express').Router();
+
+tigerRouter.get('/', function(req, res, next) {
+    console.log('tiger');
+    next();
+});
+
+module.exports = tigerRouter;
